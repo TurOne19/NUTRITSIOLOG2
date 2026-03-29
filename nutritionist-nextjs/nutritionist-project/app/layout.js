80c38a12651ae3844c2bfe8d57e52bac4bb20c15
TurOne19 <romanturovski3@gmail.com>
2026-03-30 02:08:04 +0300

    Add viewport meta tag to layout
    Added viewport meta tag for responsive design.

--- nutritionist-nextjs/nutritionist-project/app/layout.js
+++ nutritionist-nextjs/nutritionist-project/app/layout.js
@@ -10,10 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&family=DM+Sans:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&family=DM+Sans:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
       </head>
       <body>{children}</body>
     </html>
